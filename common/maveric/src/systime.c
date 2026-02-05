07cfb3b6f7bb5ff02fb2c21f63920e0b746af26a
Adhit Siripurapu <asiripur@usc.edu>
2026-02-04 18:43:53 -0800

    systime private name changes

--- common/maveric/src/systime.c
+++ common/maveric/src/systime.c
@@ -6,10 +6,10 @@
 
 // MODULE PRIVATE STATIC STATE 
 
-uint64_t s_epoch_sec_sync;
-uint64_t s_ms_sync;
-volatile uint64_t* s_irq_ms_ptr;
-struct_tm* s_rtc_ptr;
+uint64_t _s_epoch_sec_sync;
+uint64_t _s_ms_sync;
+volatile uint64_t* _s_irq_ms_ptr;
+struct_tm* _s_rtc_ptr;
 
 // SYSTIME API
 
@@ -20,26 +20,26 @@ uint64_t rtc_to_epoch_ms(struct_tm rtc) {
 }
 
 void systime_init(uint64_t* irq_ms_ptr, struct_tm* rtc_ptr) {
-    s_epoch_sec_sync = 0;
-    s_ms_sync = 0;
-    s_irq_ms_ptr = irq_ms_ptr;
-    s_rtc_ptr = rtc_ptr;
+    _s_epoch_sec_sync = 0;
+    _s_ms_sync = 0;
+    _s_irq_ms_ptr = irq_ms_ptr;
+    _s_rtc_ptr = rtc_ptr;
     systime_sync();
 }
 
 void systime_sync(void) {
-    s_epoch_sec_sync = rtc_to_epoch_ms(*s_rtc_ptr);
+    _s_epoch_sec_sync = rtc_to_epoch_ms(*_s_rtc_ptr);
     isr_disable_all();
-    s_ms_sync = *s_irq_ms_ptr;
+    _s_ms_sync = *_s_irq_ms_ptr;
     isr_enable_all();
 }
 
 
 uint64_t systime_epoch_ms(void) {
     isr_disable_all();
-    uint64_t ms_now = *s_irq_ms_ptr;
+    uint64_t ms_now = *_s_irq_ms_ptr;
     isr_enable_all();
-    uint64_t diff = ms_now - s_ms_sync;
-    uint64_t now = s_epoch_sec_sync + diff;
+    uint64_t diff = ms_now - _s_ms_sync;
+    uint64_t now = _s_epoch_sec_sync + diff;
     return now;
 }
